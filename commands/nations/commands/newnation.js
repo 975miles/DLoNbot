4ec34ab002342da4213a1feee1a1b7b4f4663d3b
Flynn <flynnamos@hotmail.com>
2018-02-25 20:16:22 +0000

    overriders should be able to create some nations
    boi

---
+++ commands/nations/commands/newnation.js
@@ -0,0 +1,29 @@
+var requireDir = require('require-dir');
+var functions = requireDir("../../../functions", {recurse: true});
+var config = require("../../../config.json");
+var c = requireDir("../../", {recurse: true});
+
+module.exports = {
+	"d":"Creates a nation for a leader. Only overriders can use this.",
+	"a":["@user"],
+	"g":"a",
+	"f":function (msg,bot,args,bal) {
+		if (config.overriders.includes(msg.author.id)) {
+			if (args[0] == undefined) {
+				msg.channel.send("Who?")
+			}
+			args[0] = args[0].replace(/\D/g,'');
+			args[1] = Math.abs(Number(args[1]));
+			if (bot.users.has(args[0])) {
+				bal.nations[args[0]] = {"relations":{},"info":"This nation has no info yet."};
+			}
+			else {
+				msg.channel.send("Not a person.");
+			}
+		}
+		else {
+			msg.channel.send("You aren't an overrider.");
+		}
+        return bal;
+	}
+}
