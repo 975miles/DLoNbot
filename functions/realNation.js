848fcfd681221b1b352102dda0c461cdd89f9fc0
Flynn <flynnamos@hotmail.com>
2018-02-26 11:13:47 +0000

    Update realNation.js

--- functions/realNation.js
+++ functions/realNation.js
@@ -2,7 +2,7 @@ var bal = require("../bal.json");
 
 module.exports = function (checking) {
 	for (var i in bal.nations) {
-		if (bal.nations[i].name == checking) {
+		if (i == checking) {
 			return true;
 		}
 	}
