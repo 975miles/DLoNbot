a5c3207b6a11278c9d1eaa7de104c6b0ebf3aab4
Flynn <flynnamos@hotmail.com>
2018-03-01 17:17:49 +0000

    Update setavatar.js

--- commands/help/commands/setavatar.js
+++ commands/help/commands/setavatar.js
@@ -6,20 +6,14 @@ var c = requireDir("../../", {recurse: true});
 module.exports = {
 	"d":"Sets my avatar. Can only be used by overriders.",
 	"a":["nick"],
-	"g":"a",
+	"g":"o",
 	"f":function (msg,bot,args,bal) {
-		if (config.overriders.includes(msg.author.id)) {
-			if (args[0] != undefined) {
-				bot.user.setAvatar(args[0]);
-				msg.channel.send("k");
-			}
-			else {
-				msg.channel.send("Set it to... what?");
-			}
+		if (args[0] != undefined) {
+			bot.user.setAvatar(args[0]);
+			msg.channel.send("k");
 		}
 		else {
-			msg.channel.send("You aren't an overrider.");
+			msg.channel.send("Set it to... what?");
 		}
-        	return bal;
 	}
 }
