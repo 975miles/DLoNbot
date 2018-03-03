e07056fba708bc94a233fb7d500ea1fbb23bce86
JPZEN <JPZEN@DESKTOP-PD85BM4>
2018-03-03 13:03:03 -0800

    asdf

---
+++ commands/time/commands/settimescale.js
@@ -0,0 +1,11 @@
+var requireDir = require('require-dir');
+var functions = requireDir("../../../functions", {recurse: true});
+
+module.exports = {
+	"d":"Sets the timescale",
+	"a":["timescale in days/day"],
+	"g":"e",
+	"f":function (msg, bot, args, bal) {
+		//oof template
+	}
+}
