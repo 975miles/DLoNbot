bf6d92aad3086f27f3f4f8727627fd5de5dd0b73
Flynn <flynnamos@hotmail.com>
2018-03-20 16:00:47 +0000

    testing

--- commands/currency/commands/listall.js
+++ commands/currency/commands/listall.js
@@ -10,12 +10,12 @@ module.exports = {
 		var output = "";
 		var lsit = [];
 		for (i in bal.currency[msg.guild.id].bank){
-			lsit.push([bal.currency[msg.guild.id].bank[i].bal,bal.currency[msg.guild.id].bank[i].name]);
+			lsit.push([bal.currency[msg.guild.id].bank[i].bal,[bal.currency[msg.guild.id].bank[i].name]]);
 		}
 		lsit.sort();
 		lsit.reverse();
 		for (var i = 0; i < lsit.length; i++){
-			output = output + "Balance of " + lsit[i][1] + ": **" + bal.currency[msg.guild.id].config.sym + lsit[i][0] + "**\n";
+			output = output + "Balance of " + lsit[i][1][0] + ": **" + bal.currency[msg.guild.id].config.sym + lsit[i][0] + "**\n";
 		}
 		if (output.length > 1995) {
 			fs.writeFile('all.txt', output);
