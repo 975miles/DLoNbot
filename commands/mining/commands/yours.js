db68e6c0a827245ba06f6a4c5118d925f54053ab
Flynn <flynnamos@hotmail.com>
2018-03-21 18:07:35 +0000

    Create yours.js

---
+++ commands/mining/commands/yours.js
@@ -0,0 +1,9 @@
+module.exports = {
+	"d":"Mine to get minerals! Can be used once per " + cooldown + " seconds.",
+	"a":[],
+	"g":"e",
+	"f":function (msg, bot, args, bal) {
+		msg.channel.send("Ｏｌｄ　ｊｏｋｅ．");
+		return bal;
+	}
+}
