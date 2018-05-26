54397bb78b87270a2c89d469c9e33c3866028f26
Flynn <flynnamos@hotmail.com>
2018-05-26 14:14:51 +0100

    sarcasm

---
+++ commands/help/commands/s.js
@@ -0,0 +1,16 @@
+var requireDir = require('require-dir');
+var functions = requireDir("../../../functions", {recurse: true});
+var config = require("../../../config.json");
+
+module.exports = {
+	"d":"Sarcasm",
+	"a":[],
+	"g":"o",
+	"f":function (msg,bot,args,bal) {
+		msg.delete();
+		for (var sarcasm = 0; sarcasm < 5; sarcasm++) {
+			msg.channel.send("THE PERSON ABOVE IS BEING SARCASTIC. DO NOT TAKE WHAT THEY SAID SERIOUSLY.")
+		}
+		return bal;
+	}
+}
