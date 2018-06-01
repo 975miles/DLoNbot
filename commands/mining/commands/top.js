b15fc3ad0aa4809e2131648a3afb56e896321026
Flynn <flynnamos@hotmail.com>
2018-06-01 21:22:21 +0100

    dumb

--- commands/mining/commands/top.js
+++ commands/mining/commands/top.js
@@ -9,15 +9,15 @@ module.exports = {
 	"f":function (msg, bot, args, bal) {
 		var output = "";
 		var lsit = [];
-		for (i in bal.mining[msg.guild.id]){
-			lsit.push([Math.round(bal.mining[msg.guild.id].money * 100) / 100, (bot.users.has(i) ? bot.users.get(i).username : i)]);
+		for (i in bal.mining){
+			lsit.push([Math.round(bal.mining[i].money * 100) / 100, (bot.users.has(i) ? bot.users.get(i).username : i)]);
 		}
 		lsit.sort(function(a,b){
 			return a[0] - b[0];
 		});
 		lsit.reverse();
 		for (var i = 0; i < lsit.length; i++){
-			output = output + (i+1) + ": Balance of " + lsit[i][1] + ": **£" + lsit[i][0] + "**\n";
+			output += (i+1) + ": Balance of " + lsit[i][1] + ": **£" + lsit[i][0] + "**\n";
 		}
 		msg.channel.send(output);
 		return bal;
