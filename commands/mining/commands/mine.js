30d2c74b9fd7abf1db925fcbed6bba832eb8a84a
Flynn <flynnamos@hotmail.com>
2018-06-02 10:11:37 +0100

    wrong shift number displays

--- commands/mining/commands/mine.js
+++ commands/mining/commands/mine.js
@@ -9,6 +9,7 @@ module.exports = {
 	"g":"e",
 	"f":function (msg, bot, args, bal) {
 		if (bal.mining[msg.author.id].nextShift < Date.now()) {
+			bal.mining[msg.author.id].shiftsCompleted++;
 			var output = "Shift **" + bal.mining[msg.author.id].shiftsCompleted + "** completed! Here's what you got:\n";
 			for (var mineral in bal.mining[msg.author.id].minerals) {
 				var adding = Math.random() * (config.months[mineral].yields.max -config.months[mineral].yields.min) + config.months[mineral].yields.min;
@@ -19,7 +20,6 @@ module.exports = {
 				output += "\n" + (functions.time(Date.now()).monthName == mineral ? mineral + " (5x month bonus)" : mineral) + ": `" + Math.round(adding * 100000) / 100000  + "g`";
 			}
 			bal.mining[msg.author.id].nextShift = Date.now() + (cooldown * 1000);
-			bal.mining[msg.author.id].shiftsCompleted++;
 			msg.channel.send(output);
 		} else {
 			msg.channel.send("You completed a shift recently. Please wait **" + Math.round((bal.mining[msg.author.id].nextShift - Date.now()) / 1000) + "** seconds to mine again.");
