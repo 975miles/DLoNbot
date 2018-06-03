06c1db36792e514d311ae7ac1af25dc6b023a85e
Flynn <flynnamos@hotmail.com>
2018-06-03 15:52:31 +0100

    Update say.js

--- commands/help/commands/say.js
+++ commands/help/commands/say.js
@@ -3,7 +3,7 @@ var functions = requireDir("../../../functions", {recurse: true});
 var config = require("../../../config.json");
 
 module.exports = {
-	"d":"Sarcasm",
+	"d":"Force the bot to say something.",
 	"a":[],
 	"g":"o",
 	"f":function (msg,bot,args,bal) {
