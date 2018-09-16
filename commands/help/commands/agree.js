c98820fc241aaa7b879d6ca616e4d739413e7be8
Flynn <flynnamos@hotmail.com>
2018-09-16 17:08:47 +0100

    e

--- commands/help/commands/agree.js
+++ commands/help/commands/agree.js
@@ -7,11 +7,12 @@ module.exports = {
 	"a":[],
 	"g":"e",
 	"f":function (msg,bot,args,bal) {
-    if (msg.guild.id == "490908587174920193") {
+		if (msg.guild.id == "490908587174920193") {
 			msg.member.addRole("490910428608593931");
 			msg.delete();
 		} else {
 			msg.channel.send("https://discord.gg/tJe6698");
 		}
+		return bal;
 	}
 }
