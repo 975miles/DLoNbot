562ae7b2606173e2eb8924bce9446fe94bba65f7
Flynn <flynnamos@hotmail.com>
2018-11-18 12:39:42 +0000

    Create date.js

---
+++ commands/date/commands/date.js
@@ -0,0 +1,19 @@
+var requireDir = require('require-dir');
+var functions = requireDir("../../../functions", {recurse: true});
+
+module.exports = {
+	"d":"Tells you the DNP time. Format: D/M/Y H:M",
+	"a":[],
+	"g":"e",
+	"f":function (msg, bot, args, bal) {
+		var date = functions.date();
+		msg.channel.send("```" + 
+			date.day + "/" + 
+			date.monthNum + "/"
+			date.year + " " +
+			date.hour + ":" +
+			date.minute + "```");
+    msg.delete();
+		return bal;
+	}
+}
