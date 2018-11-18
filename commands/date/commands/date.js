4b2957832655c9e641450dda7b26f6d419e82024
Flynn <flynnamos@hotmail.com>
2018-11-18 12:45:31 +0000

    Update date.js

--- commands/date/commands/date.js
+++ commands/date/commands/date.js
@@ -12,7 +12,7 @@ module.exports = {
 			date.monthNum + "/" +
 			date.year + " " +
 			date.hour + ":" +
-			("0" + date.minute).slice(-2); + "```");
+			("0" + date.minute).slice(-2) + "```");
     msg.delete();
 		return bal;
 	}
