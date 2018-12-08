957497fb2d81eb608bfa4d78abcadbe4f8651393
Flynn <flynnamos@hotmail.com>
2018-12-08 22:38:32 +0000

    aaaa 0th month

--- functions/date.js
+++ functions/date.js
@@ -2,7 +2,7 @@ var config = require("../config.json");
 
 module.exports = function() {
     var months = ["january","february","march","april","may","june","july","august","september","october","november","december"];
-    var calibration = (1036800000*504)+(86400000*7);
+    var calibration = (1036800000*504)+(86400000*6);
     var date = {};
     var d = new Date();
     var t = d.getTime()+calibration;
@@ -13,8 +13,8 @@ module.exports = function() {
     var minute = 2000;
     date.year = Math.floor(t/year);
     date.month = months[Math.floor(t/month)%12];
-    date.monthNum = Math.floor(t/month)%12;
-    date.day = Math.floor(t/day)%30;
+    date.monthNum = Math.floor(t/month)%12 + 1;
+    date.day = Math.floor(t/day)%30 + 1;
     date.hour = Math.floor(((t/hour)+12)%24);
     date.minute = Math.floor(t/minute)%60;
     return date;
